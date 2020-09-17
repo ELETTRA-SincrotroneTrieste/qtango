d9b7d7094cb25ecb4700d4b2698ba74deb712afd
giacomo.strangolino <giacomo.strangolino@elettra.eu>
2020-09-17 14:47:38 +0200

    fixed secs/ms issue in mixing/interpolation

--- qtango/src/plotadapter/dbcurveinterpolator.cpp
+++ qtango/src/plotadapter/dbcurveinterpolator.cpp
@@ -42,34 +42,35 @@ void DbCurveInterpolator::interpolate()
     QTime t;
     t.start();
 
-    for(size_t i = 0; i < d_ptr->curve->dataSize() - 1; i++)
+    for(size_t i = 0; i < d_ptr->curve->data()->size() - 1; i++)
     {
-     //   printf("cycle for i=%d\n", i);
         x = newxdata.last();
         /* fill with points with the same y between x(i) and x(i+1)
          * every period milliseconds until we reach next real point
          * on c
          */
-     //   while(x < c->x(i + 1))
-        while(x < c->data()->sample(i + 1).x())
+
+//        printf("DbCurveInterpolator::interpolate \e[0;32m(%s,%.1f next sample at index %d %s)\e[0m\n", qstoc(QDateTime::fromTime_t(newxdata.last()).toString()),
+//               newydata.last(), i+1, qstoc(QDateTime::fromTime_t(c->data()->sample(i + 1).x()).toString()));
+
+        x += d_ptr->period * 0.001;
+        while(0.3  < c->data()->sample(i + 1).x() - x)
         {
             newxdata << x;
-        //    newydata << c->y(i);
             newydata << c->data()->sample(i).y();
-            x = x + d_ptr->period * 0.001;
-          //  printf("\e[0;36m(%s,%.1f)\e[0m, ", qstoc(QDateTime::fromTime_t(newxdata.last()).toString()),
-           //        newydata.last());
+            printf("(\e[0;36m%s,%.1f - period %d \e[1;31mdelta %f\e[0m [x:%f sample %f])\e[0m, ", qstoc(QDateTime::fromTime_t(newxdata.last()).toString()),
+                   newydata.last(), d_ptr->period, c->data()->sample(i + 1).x() - x, x, c->data()->sample(i + 1).x());
+
+            x += d_ptr->period * 0.001;
         }
         /* place the real x,y data */
         newxdata << c->data()->sample(i + 1).x();
         newydata << c->data()->sample(i + 1).y();
-        printf("\e[0;32m(%s,%.1f)\e[0m, ", qstoc(QDateTime::fromTime_t(newxdata.last()).toString()),
-               newydata.last());
     }
-    printf("new xdata size %d ydata size %d\n\n", newxdata.size(), newydata.size());
+//    printf("DbCurveInterpolator::interpolate new xdata size %d ydata size %d\n\n", newxdata.size(), newydata.size());
 
     c->setData(newxdata, newydata);
     c->updateRawData();
 
-    printf("interpolate() took %d milliseconds\n", t.elapsed());
+//    printf("DbCurveInterpolator::interpolate interpolate() took %d milliseconds\n", t.elapsed());
 }
